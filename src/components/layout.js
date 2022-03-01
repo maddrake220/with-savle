5e733afa1939e9f5ef327387b20752ab2c2f85c2
auhy49 <auhy49@gmail.com>
2022-03-01 23:26:30 +0900

    Styling Navi bar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,15 @@
-import Navbar from './navbar';
+import Navbar from "./navbar";
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
       <main>{children}</main>
+      <style jsx>{`
+        main {
+          margin-top: 52px;
+        }
+      `}</style>
     </>
   );
 }
