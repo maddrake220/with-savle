e4614ee8bd25ae75710f6b5504f2c7ee97c771c4
auhy49 <auhy49@gmail.com>
2022-03-01 23:26:30 +0900

    Styling Navi bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -47,7 +47,10 @@ const style = css`
   }
   @media (max-width: 576px) {
     nav {
+<<<<<<< HEAD
       background-color: #f0f6fb;
+=======
+>>>>>>> fccbbd1 (Styling Navi bar)
       width: 100vw;
       height: 100vh;
       flex-direction: column;
@@ -84,6 +87,7 @@ const style = css`
       font-size: 13px;
       line-height: 20px;
     }
+<<<<<<< HEAD
     .goal,
     .vote {
       background-color: #ffffff;
@@ -91,6 +95,8 @@ const style = css`
     .saving-calc {
       background-color: #f7f8fa;
     }
+=======
+>>>>>>> fccbbd1 (Styling Navi bar)
   }
   @media (min-width: 577px) {
     nav {
@@ -118,11 +124,16 @@ const routes = [
 
 function Navbar() {
   const { sm: isMobile } = useBreakpoint();
+<<<<<<< HEAD
   const { pathname, back } = useRouter();
+=======
+  const { pathname } = useRouter();
+>>>>>>> fccbbd1 (Styling Navi bar)
   const [toggled, setToggled] = useState(false);
   const handleToggle = useCallback(() => setToggled((prev) => !prev), []);
 
   return (
+<<<<<<< HEAD
     <nav className={`${!toggled && pathname.split("/")[1]}`}>
       <div className="logoBox">
         <div className="mobile">
@@ -131,6 +142,15 @@ function Navbar() {
           <Link href="/" passHref>
             <h1 className="logo">
               <MobileLogo className="mobileLogo" fill={toggled ? "#FFFFFF" : "#3178ff"} onClick={() => setToggled(false)} />
+=======
+    <nav>
+      <div className="logoBox">
+        <div className="mobile">
+          {toggled && <Back onClick={handleToggle} />}
+          <Link href="/" passHref>
+            <h1 className="logo">
+              <MobileLogo className="mobileLogo" fill={toggled ? "#FFFFFF" : "#3178ff"} />
+>>>>>>> fccbbd1 (Styling Navi bar)
             </h1>
           </Link>
           <Bar stroke={toggled ? "#FFFFFF" : "#3178ff"} onClick={handleToggle} />
@@ -161,7 +181,11 @@ function Navbar() {
       <style jsx>{`
         nav {
           height: ${isMobile && toggled ? "100vh" : "52px"};
+<<<<<<< HEAD
           background-color: ${isMobile && toggled && "#3178ff"};
+=======
+          background-color: ${isMobile && !toggled ? "#F0F6FB" : "#3178ff"};
+>>>>>>> fccbbd1 (Styling Navi bar)
         }
         path {
           stroke: ${isMobile && !toggled ? "#3178ff" : "#FFFFFF"};
