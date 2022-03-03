1e27aeaa28715278b6428173136a4b3229b7e7c9
auhy49 <auhy49@gmail.com>
2022-03-03 17:32:09 +0900

    Fixed: navbar bg color by path

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -48,6 +48,7 @@ const style = css`
   }
   @media (max-width: 576px) {
     nav {
+      background-color: #f0f6fb;
       width: 100vw;
       height: 100vh;
       flex-direction: column;
@@ -84,6 +85,13 @@ const style = css`
       font-size: 13px;
       line-height: 20px;
     }
+    .goal,
+    .vote {
+      background-color: #ffffff;
+    }
+    .saving-calc {
+      background-color: #f7f8fa;
+    }
   }
   @media (min-width: 577px) {
     nav {
@@ -116,13 +124,13 @@ function Navbar() {
   const handleToggle = useCallback(() => setToggled((prev) => !prev), []);
 
   return (
-    <nav>
+    <nav className={`${!toggled && pathname.slice(1)}`}>
       <div className="logoBox">
         <div className="mobile">
           {toggled && <Back onClick={handleToggle} />}
           <Link href="/" passHref>
             <h1 className="logo">
-              <MobileLogo className="mobileLogo" fill={toggled ? "#FFFFFF" : "#3178ff"} />
+              <MobileLogo className="mobileLogo" fill={toggled ? "#FFFFFF" : "#3178ff"} onClick={() => setToggled(false)} />
             </h1>
           </Link>
           <Bar stroke={toggled ? "#FFFFFF" : "#3178ff"} onClick={handleToggle} />
@@ -153,7 +161,7 @@ function Navbar() {
       <style jsx>{`
         nav {
           height: ${isMobile && toggled ? "100vh" : "52px"};
-          background-color: ${isMobile && !toggled ? "#F0F6FB" : "#3178ff"};
+          background-color: ${isMobile && toggled && "#3178ff"};
         }
         path {
           stroke: ${isMobile && !toggled ? "#3178ff" : "#FFFFFF"};
