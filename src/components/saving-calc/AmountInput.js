080185d9e72b7629b7c7e3b56600910e881396e4
mirim <alfla6528@naver.com>
2022-03-04 21:22:59 +0900

    feat: add saving-amount

--- src/components/saving-calc/AmountInput.js
+++ src/components/saving-calc/AmountInput.js
@@ -1,25 +1,27 @@
 import PiggyBank from "public/layout/piggy-bank.svg";
 import PlusButton from "./PlusButton";
 import css from "styled-jsx/css";
+import { useCallback, useState } from "react";
+import comma from "@/utils/comma";
+import addAmount from "@/utils/addAmount";
 
 const style = css`
   .title {
     display: flex;
     justify-content: space-between;
-    margin-bottom: 32px;
-    height: 63px;
+    margin: 2px 0 34px;
   }
   h2 {
-    width: 143px;
+    width: 178px;
     font-weight: bold;
-    font-size: 18px;
+    font-size: 17.3px;
     line-height: 1.5;
     margin: 0;
     position: relative;
   }
   h2::before {
     content: "";
-    width: 127px;
+    width: 160px;
     height: 8px;
     background: rgba(143, 201, 255, 0.7);
     opacity: 0.4;
@@ -28,16 +30,17 @@ const style = css`
     left: -2px;
   }
   .amount {
-    margin-bottom: 53px;
+    margin-bottom: 55px;
   }
-  .saving_amount_input {
-    margin-top: 18px;
+  .saving_amount {
+    margin-top: 28px;
   }
+
   input {
     width: 175px;
     border: none;
     border-bottom: 1px solid #e3e7ed;
-    margin-right: 4px;
+    margin: 22px 4px 0 0;
     font-size: 14px;
     text-align: right;
   }
@@ -47,6 +50,7 @@ const style = css`
   input::placeholder {
     color: #b2b2b2;
   }
+
   p {
     margin: 0;
     font-size: 14px;
@@ -58,9 +62,76 @@ const style = css`
   span.hidden {
     display: none;
   }
+  button {
+    width: 220px;
+    height: 34px;
+    display: block;
+    margin: 0 auto;
+    font-weight: bold;
+    font-size: 12px;
+    color: #b2b2b2;
+    border: none;
+    border-radius: 8px;
+    background: #d6d8dc;
+  }
+  button.next {
+    color: #fff;
+    background: #3178ff;
+  }
 `;
 
-const AmountInput = ({ amount, handleChange, handleClick }) => {
+const AmountInput = (props) => {
+  const [amount, setAmount] = useState({ goal_amount: "", saving_amount: "" });
+  const { goal_amount, saving_amount } = amount;
+  const { inputs, setInputs, state, setState } = props;
+  const campareValue = Number(saving_amount.replaceAll(",", "")) < Number(goal_amount.replaceAll(",", ""));
+  const resultButtonFocus = goal_amount.length > 0 && goal_amount.length < 16 && saving_amount.length > 0 && campareValue;
+  console.log(saving_amount !== "" && !campareValue);
+  const handleChange = useCallback(
+    (e) => {
+      let { value, name } = e.target;
+      if (value == "0") return (value = " ");
+      value = comma(value);
+      setAmount({
+        ...amount,
+        [name]: value,
+      });
+    },
+    [amount],
+  );
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const { id } = e.target;
+    if (id.includes("goal")) {
+      const value = addAmount(id, goal_amount);
+      setAmount({
+        ...amount,
+        goal_amount: value,
+      });
+    } else {
+      const value = addAmount(id, saving_amount);
+      setAmount({
+        ...amount,
+        saving_amount: value,
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { id } = e.target;
+    setState({
+      ...state,
+      [id]: true,
+    });
+    setInputs({
+      ...inputs,
+      goal_amount: goal_amount,
+      saving_amount: saving_amount,
+    });
+  };
+
   return (
     <>
       <div className="title">
@@ -71,21 +142,33 @@ const AmountInput = ({ amount, handleChange, handleClick }) => {
         <div className="goal_amount">
           <p>목표 금액은</p>
           <p className="goal_amount_input">
-            <input name="goal_amount" type="text" maxLength={16} placeholder="예) 70,000,000" onChange={handleChange} value={amount}></input>원 입니다
-            <span className={amount.length > 15 ? "" : "hidden"}>*입력범위를 초과했습니다.</span>
+            <input name="goal_amount" type="text" maxLength={16} placeholder="예) 70,000,000" onChange={handleChange} value={goal_amount}></input>원 입니다
+            <span className={goal_amount.length > 15 ? "" : "hidden"}>*입력범위를 초과했습니다.</span>
           </p>
-          <PlusButton handleClick={handleClick} />
+          <PlusButton mode="goal" handleClick={handleClick} />
         </div>
         <div className="saving_amount">
           <p>저축 금액은</p>
           <p className="saving_amount_input">
-            <input name="saving_amount" type="text" maxLength={16} placeholder="예) 70,000,000" onChange={handleChange} value={amount}></input>원 입니다
-            <span className={amount.length > 15 ? "" : "hidden"}>*입력범위를 초과했습니다.</span>
+            <input
+              name="saving_amount"
+              type="text"
+              maxLength={goal_amount.length}
+              placeholder="예) 500,000"
+              onChange={handleChange}
+              value={saving_amount}
+            ></input>
+            원 입니다
+            <span className={saving_amount !== "" && !campareValue ? "" : "hidden"}>*저축금액은 목표금액을 같거나 클 수 없습니다</span>
           </p>
-          <PlusButton handleClick={handleClick} />
+          <PlusButton mode="saving" handleClick={handleClick} />
         </div>
       </div>
-    </>
+      <button id="result" onClick={handleSubmit} className={resultButtonFocus && "next"} disabled={!resultButtonFocus && "disabled"}>
+        결과보기
+      </button>
+      <style jsx>{style}</style>
+    </> //
   );
 };
 
--- src/components/saving-calc/GoalInput.js
+++ src/components/saving-calc/GoalInput.js
@@ -1,14 +1,14 @@
 import Book from "public/layout/book.svg";
+import { useCallback, useState } from "react";
 import css from "styled-jsx/css";
 const style = css`
   .title {
     display: flex;
     justify-content: space-between;
-    margin-bottom: 32px;
-    height: 63px;
+    margin-bottom: 30px;
   }
   h2 {
-    width: 143px;
+    width: 92px;
     font-weight: bold;
     font-size: 18px;
     line-height: 1.5;
@@ -17,7 +17,7 @@ const style = css`
   }
   h2::before {
     content: "";
-    width: 127px;
+    width: 92px;
     height: 8px;
     background: rgba(143, 201, 255, 0.7);
     opacity: 0.4;
@@ -26,10 +26,11 @@ const style = css`
     left: -2px;
   }
   .goal {
-    margin-bottom: 53px;
+    margin-bottom: 200px;
   }
   .goal_input {
     margin-top: 18px;
+    position: relative;
   }
   input {
     width: 188px;
@@ -46,6 +47,9 @@ const style = css`
     color: #b2b2b2;
   }
   span {
+    position: absolute;
+    top: 25px;
+    left: 0;
     font-size: 10px;
     color: red;
   }
@@ -56,13 +60,49 @@ const style = css`
     margin: 0;
     font-size: 14px;
   }
+  button {
+    width: 220px;
+    height: 34px;
+    display: block;
+    margin: 0 auto;
+    font-weight: bold;
+    font-size: 12px;
+    color: #b2b2b2;
+    border: none;
+    border-radius: 8px;
+    background: #d6d8dc;
+  }
+  button.next {
+    color: #fff;
+    background: #3178ff;
+  }
 `;
-const GoalInput = ({ goal, handleChange }) => {
+
+const GoalInput = ({ inputs, setInputs, state, setState }) => {
+  const [goal, setGoal] = useState("");
+  const nextButtonFocus = goal.length > 0 && goal.length < 21;
+  const handleChange = useCallback((e) => {
+    setGoal(e.target.value);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { id } = e.target;
+    setState({
+      ...state,
+      [id]: true,
+    });
+    setInputs({
+      ...inputs,
+      goal: goal,
+    });
+  };
+
   return (
     <>
       <div className="title">
-        <h2>저축 목표을 알려주세요.</h2>
-        <Book width="63px" height="68px " />
+        <h2>저축 목표를 알려주세요.</h2>
+        <Book width="63px" height="68px " style={{ marginRight: "7px" }} />
       </div>
       <div className="goal">
         <p>저의 목표는</p>
@@ -71,6 +111,10 @@ const GoalInput = ({ goal, handleChange }) => {
           <span className={goal.length > 20 ? "" : "hidden"}>*최대 글자수는 20자입니다.</span>
         </p>
       </div>
+      <button id="next" onClick={handleSubmit} className={nextButtonFocus && "next"}>
+        다음
+      </button>
+      <style jsx>{style}</style>
     </>
   );
 };
