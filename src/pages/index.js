912280a80ef2ee4f4c499cefc366d8c68b9bc371
mirim <alfla6528@naver.com>
2022-03-05 22:00:00 +0900

    feat: add shortcut-bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import MainVoteSection from "@/components/vote/MainVoteSection";
-
+import ShortcutBar from "@/components/ShortcutBar";
 export default function Home() {
   return (
     <>
       <MainVoteSection />
+      <ShortcutBar />
     </>
   );
 }
