7341fdc4f164477ea61de2080cc41df235b47555
mirim <alfla6528@naver.com>
2022-03-06 02:29:14 +0900

    fix: fix result

--- src/utils/periodCalc.js
+++ src/utils/periodCalc.js
@@ -1,30 +1,87 @@
 const periodCalc = (value, goal, saving) => {
   let numberGoalValue = Number(goal.replaceAll(",", ""));
   let numberSavingValue = Number(saving.replaceAll(",", ""));
-  let count = numberGoalValue / numberSavingValue;
-  let date = 0;
+  let count = Math.ceil(numberGoalValue / numberSavingValue);
+
+  let year = 0;
+  let month = 0;
+
   if (value === "month") {
     while (count > 12) {
       count = count - 12;
-      date++;
+      year++;
     }
-    if (count === 12) {
-      date++;
-      count = 0;
+    if (count === 0) {
+      return `${year}년`;
+    } else {
+      return `${year}년 ${count}개월`;
     }
-    return `${date}년 ${Math.ceil(count)}개월`;
   } else if (value === "week") {
-    while (count > 4) {
-      count = count - 4;
-      date++;
+    if (count >= 52) {
+      while (count >= 52) {
+        count = count - 52;
+        year++;
+      }
+      if (count === 0) {
+        return `${year}년`;
+      } else if (count < 4) {
+        return `${year}년 ${count}주`;
+      } else {
+        while (count >= 4) {
+          count = count - 4;
+          month++;
+        }
+        if (count === 0) {
+          return `${year}년 ${month}개월`;
+        }
+        return `${year}년 ${month}개월 ${count}주`;
+      }
+    } else if (count >= 4) {
+      while (count >= 4) {
+        count = count - 4;
+        month++;
+      }
+      if (count === 0) {
+        return `${month}개월`;
+      } else {
+        return `${month}개월 ${count}주`;
+      }
+    } else {
+      return `${count}주`;
     }
-    if (count === 4) {
-      date++;
-      count = 0;
-    }
-    return `${date}개월 ${Math.ceil(count)}주`;
   } else if (value === "day") {
-    return `${Math.ceil(count)}일`;
+    if (count >= 365) {
+      while (count >= 365) {
+        count = count - 365;
+        year++;
+      }
+      if (count === 0) {
+        return `${year}년`;
+      } else if (count < 30) {
+        return `${year}년 ${count}일`;
+      } else {
+        while (count >= 30) {
+          count = count - 30;
+          month++;
+        }
+        if (count === 0) {
+          return `${year}년 ${month}개월`;
+        }
+        return `${year}년 ${month}개월 ${count}일`;
+      }
+    } else if (count <= 365 && count > 30) {
+      while (count >= 30) {
+        count = count - 30;
+        month++;
+      }
+      if (count === 0) {
+        return `${month}개월`;
+      } else {
+        return `${month}개월 ${count}일`;
+      }
+    } else {
+      return `${count}일`;
+    }
   }
 };
 
