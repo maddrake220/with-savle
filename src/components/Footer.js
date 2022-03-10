95c29790b69c7ab46b8e2953f38d75fa11f4a650
Jinsoo Byeon <auhy49@gmail.com>
2022-03-10 23:22:06 +0900

    ADD: Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,49 @@ function Footer() {
       <h1>주식회사 부엔까미노</h1>
       <span>대표이사 이수영</span>
       <address>
-        <span>주소 서울시 영등포구 의사당대로 83 오투타워, 서울 핀테크랩 8층</span>
+        <span>
+          주소. 서울시 영등포구 의사당대로 83 <span className="address__detail">오투타워, 서울 핀테크랩 8층</span>
+        </span>
         <span>고객센터번호. 070-7537-1705</span>
-        <span>이메일. support@buencamino.io</span>
+        <span>
+          이메일.{" "}
+          <a className="black" href="mailto:support@buencamino.io">
+            support@buencamino.io
+          </a>
+        </span>
       </address>
-      <span>|서비스 이용약관|개인정보 처리방침|</span>
+      <span>
+        |{" "}
+        <a className="black" href="https://buencamino.notion.site/Savle-5128bd3f0ad245d88a655db906b36caf">
+          서비스 이용약관
+        </a>{" "}
+        |{" "}
+        <a className="black" href="https://buencamino.notion.site/6480232ee8a746258f1207bcef1c9005">
+          개인정보 처리방침
+        </a>{" "}
+        |
+      </span>
       <style jsx>{`
         footer {
-          margin-top: 60px;
-          margin-left: 165px;
+          padding-top: 60px;
+          padding-bottom: 73px;
+          padding-left: 8.59375vw;
+          padding-right: 8.59375vw;
+          line-height: 24px;
+          background: #f7f8fa;
+        }
+        address {
+          margin: 40px 0px;
+          font-style: inherit;
+        }
+        address > span {
+          display: block;
+        }
+        .address__detail {
+          display: inline-block;
+        }
+        a {
+          text-decoration: none;
         }
         h1 {
           font-size: 16px;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
+import Footer from "@/components/Footer";
 import MainGoal from "@/components/goal/MainGoal";
+import ShortcutBar from "@/components/ShortcutBar";
+import MainVoteSection from "@/components/vote/MainVoteSection";
 
 export default function Home() {
   return (
     <>
       <MainGoal />
+      <MainVoteSection />
+      <ShortcutBar />
+      <Footer />
     </>
   );
 }
