060b29e6a1de1aec538bd604aa7f7213fbe54e96
mirim <alfla6528@naver.com>
2022-03-10 20:01:36 +0900

    feat: add main saving page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import MainVoteSection from "@/components/vote/MainVoteSection";
 import ShortcutBar from "@/components/ShortcutBar";
 import Footer from "@/components/Footer";
+import MainSavingCalc from "@/components/saving-calc/MainSavingCalc";
 export default function Home() {
   return (
     <>
+      <MainSavingCalc />
       <MainVoteSection />
       <ShortcutBar />
       <Footer />
