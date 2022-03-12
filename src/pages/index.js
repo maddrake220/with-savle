80534656346b6e1609eb734dff11e7be72503c87
mirim <alfla6528@naver.com>
2022-03-13 06:50:01 +0900

    feat: add component to saving-calc-main

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Footer from "@/components/Common/Footer";
 import ShortcutBar from "@/components/Common/ShortcutBar";
 import MainGoal from "@/components/Goal/MainGoal";
+import MainSavingCalc from "@/components/saving-calc/MainSavingCalc";
 import MainVoteSection from "@/components/Vote/MainVoteSection";
 export default function Home() {
   return (
     <>
       <MainGoal />
+      <MainSavingCalc />
       <MainVoteSection />
       <ShortcutBar />
       <Footer />
