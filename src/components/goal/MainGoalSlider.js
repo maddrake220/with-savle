fee3efc22b83ac62b59f9909e42f1d410280b6ac
DESKTOP-G127V5T\dlatl <gum4019@gmail.com>
2022-03-13 01:21:48 +0900

    Fixed: (main goal) changed from svg to image

--- src/components/goal/MainGoalSlider.js
+++ src/components/goal/MainGoalSlider.js
@@ -3,6 +3,7 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import Image from "next/image";
 import Slider from "react-slick";
 import Data from "src/pages/goal/goalAPI.json";
 
@@ -14,19 +15,17 @@ export default function MainGoalSlider() {
     const { className, onClick } = properties;
     return (
       <div className={className} onClick={onClick}>
-        <svg
-          width="10"
-          height="40"
-          viewBox="0 0 11 18"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M2.17749 8.99979L1.47038 9.7069L0.763277 8.99979L1.47038 8.29268L2.17749 8.99979ZM9.11744 17.354L1.47038 9.7069L2.8846 8.29268L10.5317 15.9397L9.11744 17.354ZM1.47038 8.29268L9.11744 0.645627L10.5317 2.05984L2.8846 9.7069L1.47038 8.29268Z"
-            fill="#2D2D2D"
+          <Image
+            src="/img/prev.svg"
+            alt="이전"
+            width={10}
+            height={40}
           />
-        </svg>
         <style jsx>{`
+        .slick-arrow {
+          display: flex;
+          top: 50%;
+        }
           .slick-arrow:before {
             content: "";
           }
@@ -38,23 +37,18 @@ export default function MainGoalSlider() {
     const { className, onClick } = properties;
     return (
       <div className={className} onClick={onClick}>
-        <svg
-          width="10"
-          height="40"
-          viewBox="0 0 11 18"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M8.82251 9.00021L9.52962 8.2931L10.2367 9.00021L9.52962 9.70732L8.82251 9.00021ZM1.88256 0.646043L9.52962 8.2931L8.1154 9.70732L0.468343 2.06026L1.88256 0.646043ZM9.52962 9.70732L1.88256 17.3544L0.468343 15.9402L8.1154 8.2931L9.52962 9.70732Z"
-            fill="#2D2D2D"
+          <Image
+            src="/img/next.svg"
+            alt="next"
+            width={10}
+            height={40}
           />
-        </svg>
         <style jsx>{`
-          .slick-arrow {
-            display: flex;
-            justify-content: end;
-          }
+        .slick-arrow {
+          display: flex;
+          justify-content: end;
+          top: 50%;
+        }
           .slick-arrow:before {
             content: "";
           }
