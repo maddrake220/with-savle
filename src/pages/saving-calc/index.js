116e579d84369b80042fec9fe840260429c8ca82
mirim <alfla6528@naver.com>
2022-03-13 06:49:42 +0900

    fix: use styles.module & costom hook

--- src/pages/saving-calc/index.js
+++ src/pages/saving-calc/index.js
@@ -1,23 +1,22 @@
 import Character from "public/layout/character.svg";
-import { useState } from "react";
 
 import ShortCutBar from "@/components/Common/ShortcutBar";
 import CalcInputBox from "@/components/Saving-calc/CalcInputBox";
 import SavingCalcStep from "@/components/Saving-calc/SavingCalcStep";
-import { useBreakpoint } from "@/hooks/useBreakpoint";
+import useSavingCalc from "@/hooks/useSavingCalc";
+import useWidth from "@/hooks/useWidth";
 
 function SavingCalc() {
-  const [inputs, setInputs] = useState({ goal: "", goal_amount: "", saving_amount: "" });
-  const [state, setState] = useState({ next: false, result: false, done: false });
-  const data = { inputs, setInputs, state, setState };
-  const { sm: isMobile, md: isTablet } = useBreakpoint();
-
+  const data = useSavingCalc();
   return (
     <div style={{ background: "#f7f8fa" }}>
       <div className="container">
-        <SavingCalcStep state={state} />
+        <SavingCalcStep data={data} />
         <CalcInputBox data={data} />
-        <Character width={isMobile ? "141px " : isTablet ? "214px" : "311px"} style={{ display: "block", margin: "0 auto" }} />
+        <Character
+          width={useWidth(141, 214, 311, "px")}
+          style={{ display: "block", margin: "0 auto" }}
+        />
       </div>
       <ShortCutBar />
     </div>
