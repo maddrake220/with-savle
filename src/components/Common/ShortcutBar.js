f6c061ffd2e9a4d88c6bf83c68bd3fef7296b3c5
mirim <alfla6528@naver.com>
2022-03-13 06:46:52 +0900

    fix: use styles.module & costom hook

--- src/components/Common/ShortcutBar.js
+++ src/components/Common/ShortcutBar.js
@@ -1,94 +1,48 @@
-import css from "styled-jsx/css";
-import BackgroundBox from "public/layout/shortcut-bar-web.svg";
+import AppStore from "public/layout/ic_app-store.svg";
+import GooglePlay from "public/layout/ic_google-play.svg";
 import MobileBackgroundBox from "public/layout/shortcut-bar-mobile.svg";
 import TabletBackgroundBox from "public/layout/shortcut-bar-tablet.svg";
-import GooglePlay from "public/layout/ic_google-play.svg";
-import AppStore from "public/layout/ic_app-store.svg";
+import BackgroundBox from "public/layout/shortcut-bar-web.svg";
+
 import { useBreakpoint } from "@/hooks/useBreakpoint";
+import useWidth from "@/hooks/useWidth";
 
-const style = css`
-  .wrap {
-    position: relative;
-    box-sizing: border-box;
-    background: #f7f8fa;
-  }
+import styles from "../../../styles/ShortcutBar.module.scss";
 
-  h2 {
-    position: absolute;
-    color: #fff;
-    font-weight: bold;
-    text-align: center;
-    left: 50%;
-    transform: translate(-50%);
-    margin: 0;
-    font-size: 5vw;
-    line-height: 1.5;
-    top: 17%;
-  }
-  .shortcut {
-    position: absolute;
-    left: 50%;
-    transform: translate(-50%);
-    display: flex;
-    top: 63%;
-  }
-  .shortcut a:first-child {
-    margin-right: 6.3vw;
-  }
-  @media (min-width: 576px) {
-    .wrap {
-      padding: 0 1.1vw;
-    }
-    h2 {
-      font-size: 2.6vw;
-      line-height: 1.3;
-      top: 20%;
-    }
-    .shortcut {
-      top: 60%;
-    }
-    .shortcut a:first-child {
-      margin-right: 2.4vw;
-    }
-  }
-  @media (min-width: 1200px) {
-    .wrap {
-      padding: 0 0.9vw;
-    }
-    h2 {
-      font-size: 1.6vw;
-      line-height: 1.3;
-      top: 20%;
-    }
-    .shortcut {
-      top: 60%;
-    }
-    .shortcut a:first-child {
-      margin-right: 1.3vw;
-    }
-  }
-`;
 function ShortcutBar() {
   const { sm: isMobile, md: isTablet } = useBreakpoint();
   return (
     <>
-      <div className="wrap">
-        {isMobile && <MobileBackgroundBox width="100%" style={{ display: "block" }} />}
-        {!isMobile && isTablet && <TabletBackgroundBox width="100%" style={{ display: "block" }} />}
-        {!isMobile && !isTablet && <BackgroundBox width="100%" style={{ display: "block" }} />}
+      <div className={styles.wrap}>
+        {isMobile && (
+          <MobileBackgroundBox width="100%" style={{ display: "block" }} />
+        )}
+        {!isMobile && isTablet && (
+          <TabletBackgroundBox width="100%" style={{ display: "block" }} />
+        )}
+        {!isMobile && !isTablet && (
+          <BackgroundBox width="100%" style={{ display: "block" }} />
+        )}
         <h2>
           쉽고 FUN한 저축
           <br /> SAVLE
         </h2>
-        <div className="shortcut">
-          <a href="https://play.google.com/store/apps/details?id=io.buencamino.app.just" target="_blank" rel="noreferrer">
-            <GooglePlay width={isMobile ? "31.3vw" : isTablet ? "15.5vw" : "8.5vw"} />
+        <div className={styles.shortcut}>
+          <a
+            href="https://play.google.com/store/apps/details?id=io.buencamino.app.just"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GooglePlay width={useWidth(31.3, 15.5, 8.5, "vw")} />
           </a>
-          <a href="https://apps.apple.com/kr/app/savle-%EC%84%B8%EC%9D%B4%EB%B8%94/id1562933122" target="_blank" rel="noreferrer">
-            <AppStore width={isMobile ? "31.3vw" : isTablet ? "15.5vw" : "8.5vw"} />
+          <a
+            href="https://apps.apple.com/kr/app/savle-%EC%84%B8%EC%9D%B4%EB%B8%94/id1562933122"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AppStore width={useWidth(31.3, 15.5, 8.5, "vw")} />
           </a>
         </div>
-        <style jsx>{style}</style>
       </div>
     </>
   );
--- src/components/Common/Toggle.js
+++ src/components/Common/Toggle.js
@@ -1,52 +1,11 @@
-import css from "styled-jsx/css";
-const style = css`
-  .toggle {
-    width: 29px;
-    height: 18px;
-    border-radius: 27px;
-    background: #d6d8dc;
-    position: relative;
-    cursor: pointer;
-  }
-  .toggle::before {
-    content: "";
-    position: absolute;
-    width: 14px;
-    height: 14px;
-    top: 2px;
-    left: 2px;
-    border-radius: 50%;
-    transition: all 0.3s ease;
-    box-shadow: 0px 0.753204px 0.753204px rgba(0, 0, 0, 0.1);
-    background: #fff;
-  }
-  @media (min-width: 1200px) {
-    .toggle {
-      width: 32px;
-      height: 20px;
-      border-radius: 30px;
-    }
-    .toggle::before {
-      width: 16px;
-      height: 16px;
-      top: 2px;
-      left: 2px;
-    }
-  }
-`;
+import styles from "../../../styles/comment/Toggle.module.scss";
+
 const Toggle = ({ onClick, hidden }) => {
   return (
-    <div className="toggle" onClick={onClick}>
-      <style jsx>{style}</style>
-      <style jsx>{`
-        .toggle {
-          background: ${!hidden && "#3178ff"};
-        }
-        .toggle::before {
-          left: ${!hidden && "13px"};
-        }
-      `}</style>
-    </div>
+    <div
+      className={`${styles.toggle} ${!hidden && styles.hidden}`}
+      onClick={onClick}
+    ></div>
   );
 };
 
