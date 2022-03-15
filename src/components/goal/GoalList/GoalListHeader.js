4b96f201b81555fde47e4a251af65b7598428c88
maddrake220 <jwchoi4118@gmail.com>
2022-03-15 13:23:42 +0900

    feat: seo

--- src/components/goal/GoalList/GoalListHeader.js
+++ src/components/goal/GoalList/GoalListHeader.js
@@ -1,17 +1,24 @@
-import Head from "next/head";
 import Image from "next/image";
 import React from "react";
 import styles from "styles/goal/GoalList.module.scss";
 
+import Seo from "@/components/Common/Seo";
 import { getSize } from "@/utils/goal/functions";
 export default function GoalListHeader({ queryMatch }) {
   return (
     <header>
-      <Head>
-        <title>savle 목표달성</title>
-        <meta keyword="" />
-        <meta contents="" />
-      </Head>
+      <Seo
+        title={"목표공유 | 쉽고 FUN한 저축, 세이블"}
+        keyword={("목표달성", "목표등록", "저축", "공유", "세이블")}
+        desc={
+          "세이블에서 목표달성, 함께해요! 다른 사람들과 목표를 공유해보아요."
+        }
+        ogUrl={`https://savle.net/goal`}
+        ogTitle={"저축러의 목표공유"}
+        ogDesc={
+          "세이블에서 목표달성, 함께해요! 다른 사람들과 목표를 공유해보아요."
+        }
+      />
       <div className="container">
         <div className={`${styles.headerInfo}`}>
           <h1>세이블에서 목표달성, 함께해요!</h1>
