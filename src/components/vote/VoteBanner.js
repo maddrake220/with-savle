920b0c63fb7361724df361d624f10962d91af9fe
talentforest <talentforest0501@gmail.com>
2022-03-16 14:06:33 +0900

    VoteBanner Copy bug fix

---
+++ src/components/vote/VoteBanner.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+import style from "@/styles/vote/VoteIndex.module.scss";
+
+export default function Banner() {
+  return (
+    <section className={style.banner_box_container}>
+      <h1 className={style.banner_box_title}>저축러의 고민해결소</h1>
+      <p className={style.banner_box_sub}>
+        저축에 관한 고민을 나누고
+        <br />
+        투표하며 함께 고민을 해결해요.
+      </p>
+      <div className={style.img_character_money} width={82} height={67}>
+        <Image
+          className={style.character_money}
+          layout="responsive"
+          src="/img/char.svg"
+          alt="character"
+          width={82}
+          height={67}
+          priority={true}
+        />
+      </div>
+    </section>
+  );
+}
